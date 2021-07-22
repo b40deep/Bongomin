92c08b0fbec5f550f8781d347ce9d92c6ffc8943
b40deep@NettyzAir <boydtwine@gmail.com>
2021-07-22 17:51:52 +0300

    added initial get and post functionality

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser'); 
 const app = express();
 const port = process.env.PORT || 3001;
 const cors = require('cors');
@@ -6,14 +7,21 @@ const cors = require('cors');
 const allowedOrigins = [ 'http://localhost:3000' ];
 const options = (cors.CorsOptions = { origin: allowedOrigins });
 app.use(cors(options));
+app.use(bodyParser.json({limit:"10mb",extended:true}));
+app.use(bodyParser.urlencoded({limit:"10mb",extended:true}));
+
+let testList=[{"id":1,"name":"Kampala"},{"id":2,"name":"Jinja"},{"id":3,"name":"Mbale"}];
 
 app.get('/testing', (req, res) => {
+	res.json(testList);
 	res.send('Get backend Hello World!');
 });
 
-// app.post('/', (req, res) => {
-// 	res.send('Post backend Hello World!');
-// });
+app.post('/createpost', (req, res) => {
+	console.log('these are they',req.body);
+	res.json(req.body);
+	res.end();
+});
 
 app.listen(port, () => {
 	console.log(`Bongomin backend listening on port ${port}`);
--- frontend/src/components/CreatePost.jsx
+++ frontend/src/components/CreatePost.jsx
@@ -1,79 +1,86 @@
-import Navbar from './Navbar';
-import Menu from './Menu';
-import { Link } from 'react-router-dom';
-import '../css/Post.css';
-import React, { useState } from 'react';
+import Navbar from "./Navbar";
+import Menu from "./Menu";
+import { Link } from "react-router-dom";
+import "../css/Post.css";
+import React, { useState } from "react";
 
 const CreatePost = () => {
-	let userName = 'SoAndSo';
-	let userLocation = 'Rich people apartments, Muyenga';
-	let isDonating = false;
-	let postType = isDonating ? 'donate' : 'request';
-	let handleSubmit = () => {};
-	let handleAddInput = () => {
-		console.log(newPostItems);
-		setNewPosItems([ ...newPostItems, `input${newPostItems.length + 1}` ]);
-	};
-	let [ newPostItems, setNewPosItems ] = useState([ 'input1' ]);
-	let newPostItemsList = newPostItems.map((post, index) => (
-		<input
-			type="text"
-			key={index}
-			id={post}
-			className="form-control mt-3"
-			placeholder={`type a ${postType} here`}
-			name={post}
-			onChange={handleSubmit}
-		/>
-	));
-	return (
-		<div>
-			<Navbar />
-			<div className="container ">
-				<div className="row-cols-1">
-					<div className="m-auto col-12 col-sm-10 col-md-8 col-lg-6 col-xl-6 col-xxl-6">
-						<div className="card mt-5">
-							<div className="card-header bg-white d-grid">
-								<span className="fst-italic mx-2">
-									{userName} from {userLocation} has this {postType}:
-								</span>
-							</div>
-							<div className="card-body ">
-								<div className="d-grid">
-									<form>
-										<div className="form-outline mb-4 mt-3 d-grid">
-											{newPostItemsList}
-											<button
-												className="btn btn-sm btn-outline-secondary fst-italic  mt-3 "
-												type="button"
-												onClick={handleAddInput}
-											>
-												add another {postType} item
-											</button>
-										</div>
-										<Link to="/profile" className="nolinkcolor">
-											{' '}
-											<div className="d-grid">
-												<button
-													className="btn btn-outline-secondary fst-italic fs-5 mt-3 "
-													type="button"
-													onClick={handleSubmit}
-												>
-													create {postType}!
-												</button>
-											</div>
-										</Link>
-									</form>
-								</div>
-							</div>
-							<div className="d-grid" />
-						</div>
-					</div>
-				</div>
-			</div>
-			<Menu />{' '}
-		</div>
-	);
+  let userName = "SoAndSo";
+  let userLocation = "Rich people apartments, Muyenga";
+  let isDonating = false;
+  let postType = isDonating ? "donate" : "request";
+  let handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target.type);
+  };
+  let handleAddInput = () => {
+    console.log(newPostItems);
+    setNewPosItems([...newPostItems, `input${newPostItems.length + 1}`]);
+  };
+  let [newPostItems, setNewPosItems] = useState(["input1"]);
+  let newPostItemsList = newPostItems.map((post, index) => (
+    <input
+      type="text"
+      form="listForm"
+      key={index}
+      id={post}
+      className="form-control mt-3"
+      placeholder={`type a ${postType} here`}
+      name={post}
+      onChange={handleSubmit}
+    />
+  ));
+  return (
+    <div>
+      <Navbar />
+      <div className="container ">
+        <div className="row-cols-1">
+          <div className="m-auto col-12 col-sm-10 col-md-8 col-lg-6 col-xl-6 col-xxl-6">
+            <div className="card mt-5">
+              <div className="card-header bg-white d-grid">
+                <span className="fst-italic mx-2">
+                  {userName} from {userLocation} has this {postType}:
+                </span>
+              </div>
+              <div className="card-body ">
+                <div className="d-grid">
+                  <form
+                    // name="listForm"
+                    action="http://localhost:3001/createpost"
+                    method="POST"
+                  >
+                    <div className="form-outline mb-4 mt-3 d-grid">
+                      {newPostItemsList}
+                      <button
+                        className="btn btn-sm btn-outline-secondary fst-italic  mt-3 "
+                        type="button"
+                        onClick={handleAddInput}
+                      >
+                        add another {postType} item
+                      </button>
+                    </div>
+                    {/* <Link to="/profile" className="nolinkcolor"> */}{" "}
+                    <div className="d-grid">
+                      <button
+                        className="btn btn-outline-secondary fst-italic fs-5 mt-3 "
+                        type="submit"
+                        // onClick={handleSubmit}
+                      >
+                        create {postType}!
+                      </button>
+                    </div>
+                    {/* </Link> */}
+                  </form>
+                </div>
+              </div>
+              <div className="d-grid" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <Menu />{" "}
+    </div>
+  );
 };
 
 export default CreatePost;
