b7fa0435fa9c505d991ec1c32d5fa470e89a05bf
Migisha Boyd <boydtwine@gmail.com>
2021-07-24 14:36:40 +0300

    added MongoDB pipeline

--- frontend/src/components/CreatePost.jsx
+++ frontend/src/components/CreatePost.jsx
@@ -1,6 +1,6 @@
 import Navbar from './Navbar';
 import Menu from './Menu';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import '../css/Post.css';
 import React, { useState } from 'react';
 import axios from 'axios';
@@ -9,20 +9,30 @@ const CreatePost = () => {
 	let userName = 'SoAndSo';
 	let userLocation = 'Rich people apartments, Muyenga';
 	let isDonating = false;
-	let postType = isDonating ? 'donate' : 'request';
+	let postType = isDonating ? 'donation' : 'request';
 	let [ newInputs, setNewInputs ] = useState([ 'input1' ]);
 	let [ newItems, setNewItems ] = useState([]);
 	let handleNewItem = (e) => {
 		e.preventDefault();
 		let temp = newItems;
-		temp.push(e.target.value);
-		temp = temp.filter((entry) => entry !== e.target.value.substring(0, e.target.value.lenth - 1));
-
-		console.log('temp', temp);
+		console.log('old temp', temp);
+		temp[e.target.id] = e.target.value;
+		setNewItems(newItems, temp);
+		console.log('temp', newItems);
 	};
 	let handleAddInput = () => {
 		setNewInputs([ ...newInputs, `input${newInputs.length + 1}` ]);
 		console.log(newInputs);
+	};
+	let handleSubmit = () => {
+		axios.post('http://localhost:3001/posts', {
+			items: newItems,
+			post_id: 404,
+			type: 'Request',
+			location: 'Eidinda',
+			time: 'top of the hour'
+		});
+		console.log('data submitted to MongoDB sussessuwy');
 	};
 	let newPostItemsList = newInputs.map((post, index) => (
 		<input
@@ -51,9 +61,9 @@ const CreatePost = () => {
 							<div className="card-body ">
 								<div className="d-grid">
 									<form
-										// name="listForm"
-										action="http://localhost:3001/createpost"
-										method="POST"
+									// name="listForm"
+									// action="http://localhost:3001/createpost"
+									// method="POST"
 									>
 										<div className="form-outline mb-4 mt-3 d-grid">
 											{newPostItemsList}
@@ -69,8 +79,8 @@ const CreatePost = () => {
 										<div className="d-grid">
 											<button
 												className="btn btn-outline-secondary fst-italic fs-5 mt-3 "
-												type="submit"
-												// onClick={handleSubmit}
+												type="button"
+												onClick={handleSubmit}
 											>
 												create {postType}!
 											</button>
