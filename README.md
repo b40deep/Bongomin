e1a078dd8a8b0b87e4d3b98c1cc9c6ec158a8314
Migisha Boyd <boydtwine@gmail.com>
2021-07-27 17:41:18 +0300

    updated gitignore entries to remove node_modules

--- README.md
+++
@@ -1,9 +0,0 @@
-# Bongomin
-A location-based app that connects those who need something and those who have something to give.
-
-## why Bongomin exists
-
-## how Bongomin works
-
-## repo structure
-
--- backend/routes/postRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const postController = require('../controllers/postController.js');
-const postModel = require('../models/postModel.js');
-
-const postRouter = express.Router();
-
-postRouter.get('/', postController.getPosts);
-postRouter.post('/', postController.createPost);
-module.exports = postRouter;
